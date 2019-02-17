d974009d69f9b37b3a2f561b1d5261846f65cf52
Theodore Kimana <kimana.theodore@gmail.com>
2019-02-17 19:28:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,9 +1,9 @@
 def game_hash
   :home =>{
     :away =>{
-    :team_name => {" Jemo","Theo","Alex"}
+    :team_name{"Simba"}
     :colers => {" top White", " bottom Red"}
-    :
+    :players =>{"Jemo","Cobi","Alex"}
     }
   }
 
